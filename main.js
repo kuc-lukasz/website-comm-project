64f9a88a99f4dd107a9a456eda32388fb80b3032
Luka Ku <lukaszkuc93@gmail.com>
2021-10-29 18:34:56 +0200

    updates icons, html and css

--- main.js
+++ main.js
@@ -5,53 +5,54 @@ const missionBtn = document.querySelector('.nav-link-mission')
 
 const partnerZone = document.querySelector('.partner-zone')
 const mainCont = document.querySelector('.main-container')
-const invZone = document.querySelector(".inv-zone")
+const invZone = document.querySelector(".investor-zone")
 const innovZone = document.querySelector('.innov-zone')
 const missionZone = document.querySelector('.mission-zone')
 
 
 partnerBtn.addEventListener('click', () => {
-    partnerZone.classList.add('view')
     partnerZone.classList.remove('left')
-    mainCont.classList.add('left')
-    invZone.classList.add('left')
-    innovZone.classList.add('left')
-    missionZone.classList.add('left')
-    missionZone.classList.remove('view')
+
+        mainCont.classList.add('left')
+        invZone.classList.add('left')
+        innovZone.classList.add('left')
+        missionZone.classList.add('left')
+
+        partnerZone.classList.add('view')
 })
 
 investorBtn.addEventListener('click', ()=> {
+    invZone.classList.remove('left')
+
     mainCont.classList.add('left')
     partnerZone.classList.add('left')
-    partnerZone.classList.remove('view')
-    invZone.classList.remove('left')
-    invZone.classList.add('view')
     innovZone.classList.add('left')
     missionZone.classList.add('left')
-    missionZone.classList.remove('view')
+
+    invZone.classList.add('view')
+
 })
 
 innovationBtn.addEventListener('click', ()=> {
-    mainCont.classList.add('left')
-    partnerZone.classList.add('left')
-    partnerZone.classList.remove('view')
-    invZone.classList.remove('view')
-    invZone.classList.add('left')
     innovZone.classList.remove('left')
-    innovZone.classList.add('view')
-    missionZone.classList.add('left')
-    missionZone.classList.remove('view')
+
+        mainCont.classList.add('left')
+        partnerZone.classList.add('left')
+        invZone.classList.add('left')
+        missionZone.classList.add('left')
+
+        innovZone.classList.add('view')
+
 })
 
 missionBtn.addEventListener('click', ()=>{
-    mainCont.classList.add('left')
-    partnerZone.classList.add('left')
-    partnerZone.classList.remove('view')
-    invZone.classList.add('left')
-    invZone.classList.remove('view')
-    innovZone.classList.add('left')
-    innovZone.classList.remove('view')
     missionZone.classList.remove('left')
-    missionZone.classList.add('view')
-})
 
+        mainCont.classList.add('left')
+        partnerZone.classList.add('left')
+        invZone.classList.add('left')
+        innovZone.classList.add('left')
+
+        missionZone.classList.add('view')
+
+})
